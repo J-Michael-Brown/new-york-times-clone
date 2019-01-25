bfc5d0c99f776154c216efbdceedd5939c4fd57f
John Michael Brown <jmichaelbrown132737@gmail.com>
2019-01-25 11:06:25 -0800

    display article as home page

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { loremArticle } from '../../assets/lorem-article';
+import { Article } from '../models/article.model';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
-export class ArticleComponent implements OnInit {
+export class ArticleComponent{
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  article: Article = loremArticle;
 
 }
