802bed4c9472cc902a9c7613f152e94dcefac3f0
John Michael Brown <jmichaelbrown132737@gmail.com>
2019-01-25 11:26:30 -0800

    add method for displaying the full date of the article

--- src/app/models/article.model.ts
+++ src/app/models/article.model.ts
@@ -2,7 +2,12 @@ import { Author } from './author.model';
 import { SectionPiece } from './section-piece.model';
 
 class Article {
+  months: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
   constructor(public title: string, public date: Date, public author: Author, public sectionPieces: SectionPiece[]) {}
+
+  getDate() {
+    return (this.months[this.date.getUTCMonth()] + '. ' + this.date.getUTCDate() + ', ' + this.date.getUTCFullYear())
+  }
 }
 
 export { Article }
