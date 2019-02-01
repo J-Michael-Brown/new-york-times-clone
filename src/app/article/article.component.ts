708718f18c88ae970bb08c7aa8716e8a07aaae1d
John Michael Brown <jmichaelbrown132737@gmail.com>
2019-02-01 10:38:06 -0800

    add single article display (date is bugged)

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
-import { loremArticle } from '../../assets/lorem-article';
-import { Article } from '../models/article.model';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { ArticlesService } from '../articles.service';
+import { Article, getDate } from 'app/models/article.model';
+
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.css'],
+  providers: [ArticlesService]
 })
-export class ArticleComponent{
+export class ArticleComponent implements OnInit {
 
-  newsArticle: Article = loremArticle;
+  articleToDisplay;
+
+  constructor(private route: ActivatedRoute, private location: Location, private articlesService: ArticlesService) {}
+
+  ngOnInit() {
+    let articleId: string;
+    this.route.params.forEach((urlParameters)=> {
+
+    articleId = urlParameters['id'];
+    });
+    this.articleToDisplay = this.articlesService.getArticleById(articleId);
+  }
 
 }
