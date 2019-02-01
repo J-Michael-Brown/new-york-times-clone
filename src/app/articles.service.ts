b85accc80a33ae6d9f16f37b6fa3c99072280993
John Michael Brown <jmichaelbrown132737@gmail.com>
2019-02-01 10:07:20 -0800

    add articles service, with methods

--- src/app/articles.service.ts
+++ src/app/articles.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Article } from './models/article.model';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Injectable()
 export class ArticlesService {
 
-  constructor() { }
+  articles: FirebaseListObservable<any[]>;
 
+  constructor(private database: AngularFireDatabase) {
+    this.articles = database.list('articles');
+  }
+  getArticles() {
+    return this.articles;
+  }
+  addArticle(newArticle: Article) {
+    this.articles.push(newArticle)
+  }
+  getArticleById(articleId: string) {
+    return this.database.object('articles/' + articleId)
+  }
+  // updateArticle(localUpdatedArticle){
+  //   var articleEntryInFirebase = this.getArticleById(localUpdatedArticle.$key);
+  //   articleEntryInFirebase.update({title: localUpdatedArticle.title,
+  //                               artist: localUpdatedArticle.artist,
+  //                               description: localUpdatedArticle.description});
+  // }
+  deleteArticle(localArticleToDelete){
+    var articleEntryInFirebase = this.getArticleById(localArticleToDelete.$key);
+    articleEntryInFirebase.remove();
+  }
 }
